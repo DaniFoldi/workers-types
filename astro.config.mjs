d6eea772435a8d8747f109a500429ed52bbad854
Dániel Földi <dani@stefan.hu>
2023-05-02 22:05:50 +0100

    :paperclip: Use astro-critters to inline styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,51 +1,45 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import compress from 'astro-compress';
-
+import critters from "astro-critters";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://workers-types.pages.dev',
   base: '/',
   trailingSlash: 'never',
-
   output: 'static',
-
   integrations: [
     tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
+    config: {
+      applyBaseStyles: false
+    }
+  }), 
     image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    mdx(),
-
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), 
+    mdx(), 
     compress({
-      css: true,
-      html: {
-        removeAttributeQuotes: false,
-      },
-      img: false,
-      js: true,
-      svg: true,
-
-      logger: 1,
-    }),
-  ],
-
+    css: true,
+    html: {
+      removeAttributeQuotes: false
+    },
+    img: false,
+    js: true,
+    svg: true,
+    logger: 1
+  }),
+    critters()],
   vite: {
     resolve: {
       alias: {
-        '~': resolve(__dirname, './src'),
-      },
-    },
-  },
+        '~': resolve(__dirname, './src')
+      }
+    }
+  }
 });
